765ba965355165dcd39817e5271302e7d74d31c5
Billionaire123456 <fortunedivine04@gmail.com>
2022-10-17 02:29:00 -0700

    wahala

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main -print a line of code using puts
+ * main- Serves as the entry point for the program
  *
- * Return: o
+ * Return: By default returns zero if no errors otherwise return non-zero value
  */
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\ "programming is like building a multilingual puzzle");
 	return (0);
 }
